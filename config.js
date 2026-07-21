07047fe40382f5051e6bd09a4ed7de726b287624
BiroPendidikan <47309095+BiroPendidikan@users.noreply.github.com>
2026-07-21 08:25:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxs2N4o6TqFanf-Ulhe_Wkr73XLimKfC3TN-UI0kNHWrwWxLW6op0uBvt26_dRB7rL9Bg/exec', // Ganti dengan URL Web App
+  API_URL: 'https://script.google.com/macros/s/AKfycbzmqiOlXwOgc4NeKvCCZy3gSxyggNXv0g1r7dW9ZCyhQL1G5byPBCuNgJRjsR5zz2IELA/exec', // Ganti dengan URL Web App
   APP_NAME: 'Sistem TP KSSR'
 };
